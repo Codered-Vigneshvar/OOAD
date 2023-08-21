75b410a56524f339e806bb942c11d0653b1dc7c0
Codered-Vigneshvar <94245160+Codered-Vigneshvar@users.noreply.github.com>
2023-08-21 12:49:38 +0530

    Add files via upload

---
+++ Exam Registration Portal/Computer.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Computer {
+
+    /**
+     * Default constructor
+     */
+    public Computer() {
+    }
+
+    /**
+     * 
+     */
+    public void Student Record;
+
+    /**
+     * 
+     */
+    public void Booklist;
+
+    /**
+     * 
+     */
+    public void MaintainStudentRecord() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void enter issue() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void order new author() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void check availability() {
+        // TODO implement here
+    }
+
+}
