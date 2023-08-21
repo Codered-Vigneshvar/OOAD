00e326c7ed1349e23417732503bf19d6204b2b20
Codered-Vigneshvar <94245160+Codered-Vigneshvar@users.noreply.github.com>
2023-08-21 10:41:49 +0530

    Add files via upload

---
+++ Passport Management/Applicant.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Applicant {
+
+    /**
+     * Default constructor
+     */
+    public Applicant() {
+    }
+
+    /**
+     * 
+     */
+    public void Applicant name;
+
+    /**
+     * 
+     */
+    public void Applicant details;
+
+    /**
+     * 
+     */
+    public void apply passport() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void submissionofproof() {
+        // TODO implement here
+    }
+
+}
