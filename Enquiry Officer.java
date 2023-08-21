451bcc6b63d343ba20f8d50ca68d52ee044dde8f
Codered-Vigneshvar <94245160+Codered-Vigneshvar@users.noreply.github.com>
2023-08-21 10:12:34 +0530

    Add files via upload

---
+++ Enquiry Officer.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Enquiry Officer {
+
+    /**
+     * Default constructor
+     */
+    public Enquiry Officer() {
+    }
+
+    /**
+     * 
+     */
+    public void verification of details;
+
+    /**
+     * 
+     */
+    public void issue of passport() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void renewal of passport() {
+        // TODO implement here
+    }
+
+}
---
+++ Passport Management System.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Passport Management System {
+
+    /**
+     * Default constructor
+     */
+    public Passport Management System() {
+    }
+
+    /**
+     * 
+     */
+    public void veryDetail;
+
+    /**
+     * 
+     */
+    public void storeproof;
+
+    /**
+     * 
+     */
+    public void Attribute2;
+
+    /**
+     * 
+     */
+    public void issue of passport() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void verification of passport() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void cancellation of passport() {
+        // TODO implement here
+    }
+
+}
