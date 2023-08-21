80823b13311d02c0979f310d2c2cc8d68be606e5
Codered-Vigneshvar <94245160+Codered-Vigneshvar@users.noreply.github.com>
2023-08-21 12:47:07 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Student {
+
+    /**
+     * Default constructor
+     */
+    public Student() {
+    }
+
+    /**
+     * 
+     */
+    public void student details;
+
+    /**
+     * 
+     */
+    public void request for books() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void return previous books() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void register() {
+        // TODO implement here
+    }
+
+}
